123f3f967bd7694153b0335a711dbe2fac00cc83
Aggie123456 <lwensisiagnes@gmail.com>
2025-02-04 20:10:51 +0300

    Add files via upload

---
+++ JAVASCRIPT/main.js
@@ -0,0 +1,47 @@
+let name ="Aggie";
+console.log(name);
+let age = 22;
+ console.log(age);
+
+// /*Regular arrays*/ 
+ let students =['AGGIE','CATHY','FLO','TIMO'];
+console.log(students);//outputting all
+//Outputting one result
+ console.log(students[1]);
+
+// //pop method built in method removes the last item in an array
+ students.pop();
+console.log(students);
+
+// //shift method builtin method removes the first item in an array and executes them
+students.shift();
+ console.log(students);
+
+// //unshift method identify an item you want to add in the array OR Add an iten and appears to be the first
+students.unshift('LINDA');
+ console.log(students);
+
+ //push add an item an dappears to the end
+students.push("ANN");
+console.log(students);
+
+let BBC =[]; //An empty array
+BBC.push('Aggie','Vanessa');
+console.log(BBC);
+
+//ASSOCIATIVE ARRAYS
+let items ={
+    'fruit':['apple','mango','orange'],
+    'vegatables':'cabbages',
+    'drinks':'water',
+};
+console.log(items.fruit);
+console.log(vegatables);
+console.log(drinks);
+
+
+let phones={
+    'camon':'camon 30',
+    'spark':'spark 30',
+};
+
